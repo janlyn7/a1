be4710891b4ed09422b863112670373f7e823ac9
Janine Lyn <jlyn@cfa.harvard.edu>
2017-02-06 22:18:07 -0500

    New project Intro with image

---
+++ index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <link href='aboutme.css' rel='stylesheet' type='text/css'>
+    <link href='https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css' rel='stylesheet' type='text/css'/>
+    <link href='https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css' rel='stylesheet' type='text/css'>
+
+<title>Janine Lyn - Assignment 1</title>
+
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="twelve columns">  
+	<h1>Janine Lyn</h1>
+
+	<img src="images/janine.jpg" width=400 alt="image">
+
+
+	<h2>About Me</h2>
+
+
+	<h2>Random Quote</h2>
+      </div>
+    </div>
+</body>
