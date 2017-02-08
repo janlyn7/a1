b57d60349cbdf1e18e380137312028afae7f2672
Janine Lyn <jlyn@cfa.harvard.edu>
2017-02-07 22:24:33 -0500

    Updated stylesheet - added fonts, background, photo shadow Add About Me content

--- index.php
+++ index.php
@@ -1,29 +1,49 @@
+<?php
+$quotes = array ('Live long, and prosper.',
+    'Logic is the beginning of wisdom, not the end.',
+    'Insufficient facts always invite danger.', 
+    'The needs of the many outweigh the needs of the few or the one.', 
+    'When you eliminate the impossible, whatever remains, however improbable, must be the truth.', 
+    'Without followers, evil cannot spread.')
+?>
+
+
+
 <!DOCTYPE html>
 <html>
 
-<head>
+
+  <head>
     <meta charset="utf-8" />
+    <link rel="icon" type="image/png" href="JL_logo.jpg"> 
     <link href='aboutme.css' rel='stylesheet' type='text/css'>
-    <link href='https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css' rel='stylesheet' type='text/css'/>
+    <link href='https://fonts.googleapis.com/css?family=Arima+Madurai' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Cormorant+Infant' rel='stylesheet' type='text/css'>
+    <link href='https://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
+    <link href='https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.min.css' rel='stylesheet' type='text/css' />
     <link href='https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css' rel='stylesheet' type='text/css'>
 
-<title>Janine Lyn - Assignment 1</title>
+    <title>Janine Lyn - Assignment 1</title>
+  </head>
 
-</head>
+  <body>
+    <div class="container" id="about_content">
+          <h2>Janine Lyn</h2>
 
-<body>
-  <div class="container">
-    <div class="row">
-      <div class="twelve columns">  
-	<h1>Janine Lyn</h1>
+          <img src="images/janine.jpg" alt="Photo: Ancient Roman Road, Sierra de Gredos, Spain, 2000">
 
-	<img src="images/janine.jpg" width=400 alt="image">
+	  <br/>
+	  <br/>
+          <h4>About Me</h4>
+          <p>
+           Hello!  I'm a software developer residing near Boston, MA. I work for a space-based satellite called the Chandra X-Ray Observatory, writing software to assist with data processing and data analysis.  In my spare time, I enjoy ballroom dancing, martial arts, photgraphy, travel, and spending time with my two cats. 
+          </p>
 
-
-	<h2>About Me</h2>
-
-
-	<h2>Random Quote</h2>
-      </div>
+	  <br/>
+          <h4>Random Quote</h4>
+          <p>
+	    <?php echo $quotes[array_rand($quotes, 1)]; ?>
+          </p>
     </div>
-</body>
+  </body>
