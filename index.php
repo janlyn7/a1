c22da2426e1a3741a3de0a754e4dee756008347e
Janine Lyn <jlyn@cfa.harvard.edu>
2017-02-09 01:41:08 -0500

    Added tab icon, updated About Me and Random Quote content

--- index.php
+++ index.php
@@ -1,12 +1,9 @@
 <?php
-$quotes = array ('Live long, and prosper.',
-    'Logic is the beginning of wisdom, not the end.',
-    'Insufficient facts always invite danger.', 
-    'The needs of the many outweigh the needs of the few or the one.', 
-    'When you eliminate the impossible, whatever remains, however improbable, must be the truth.', 
-    'Without followers, evil cannot spread.')
+$quotes = array ( 'I have not failed. I\'ve just found 10,000 ways that won\'t work. <br/>- Thomas Alva Edison',
+	  	  'I don\'t believe in astrology; I\'m a Sagittarian and we\'re skeptical. <br/>- Arthur C. Clarke',
+		  'Electricity is really just organized lightning. <br/>- George Carlin',
+		  'Only two things are infinite, the universe and human stupidity, and I\'m not sure about the universe. <br/>- Albert Einstein')
 ?>
-
 
 
 <!DOCTYPE html>
@@ -14,7 +11,7 @@ $quotes = array ('Live long, and prosper.',
 
   <head>
     <meta charset="utf-8" />
-    <link rel="icon" type="image/png" href="JL_logo.png"> 
+    <link rel="icon" type="image/png" href="images/letter_j.png"> 
     <link href='aboutme.css' rel='stylesheet' type='text/css'>
 
     <link href='https://fonts.googleapis.com/css?family=Cormorant+Infant' rel='stylesheet' type='text/css'>
@@ -28,21 +25,31 @@ $quotes = array ('Live long, and prosper.',
 
   <body>
     <div class="container" id="about_content">
-          <h2>Janine Lyn</h2>
 
-          <img src="images/janine.jpg" alt="Photo: Ancient Roman Road, Sierra de Gredos, Spain, 2000">
+    	 <h2>Janine Lyn</h2>
+         <img src="images/janine.jpg" title="Ancient Roman Road, Sierra de Gredos, Spain, 2000">
+
+	 <br/><br/>
+
+         <h4>About Me</h4>
+         <div id="about_me">
+           Hello!  I'm a software developer residing near Boston, MA. I studied Planetary and Space Sciences at 
+           Boston University and have worked on various satellite projects for the past 20 years. 
+           I currently work for the Chandra X-Ray Observatory, writing software to assist with data processing and 
+           analysis, mainly in C, C++, and Python.  
+	   <br/><br/>
+	   I'm interested in web design and development because it's a great way for me to combine my analytical 
+	   and creative sides and will hopefully lead to other opportunities.
+	   <br/><br/>
+           In my spare time, I enjoy ballroom dancing, martial arts, photography, travel, and spending time with 
+           my two mischievous cats. 
+          </div>
 
 	  <br/>
-	  <br/>
-          <h4>About Me</h4>
-          <p>
-           Hello!  I'm a software developer residing near Boston, MA. I work for a space-based satellite called the Chandra X-Ray Observatory, writing software to assist with data processing and data analysis.  In my spare time, I enjoy ballroom dancing, martial arts, photgraphy, travel, and spending time with my two cats. 
-          </p>
 
-	  <br/>
           <h4>Random Quote</h4>
-          <p>
+          <div id="quote">
 	    <?php echo $quotes[array_rand($quotes, 1)]; ?>
-          </p>
+          </div>
     </div>
   </body>
